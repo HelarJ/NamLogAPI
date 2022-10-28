e2b190eb5d519889fe762a4ab14cb9474d903322
Helar Jaadla <helar@jaadla.com>
2022-10-28 16:12:05 +0300

    Add status endpoint.

--- src/main/java/com/jaadla/namlogapi/controller/LogController.java
+++ src/main/java/com/jaadla/namlogapi/controller/LogController.java
@@ -2,6 +2,8 @@ package com.jaadla.namlogapi.controller;
 
 import com.jaadla.namlogapi.model.Message;
 import com.jaadla.namlogapi.service.LogService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CommonsLog
 @RestController
 @RequestMapping(path = "/api/log", produces = MediaType.APPLICATION_JSON_VALUE)
 public class LogController {
@@ -25,8 +27,20 @@ public class LogController {
     Page<Message> getLog(
         @PathVariable String username,
         Pageable pageable) {
-        return logService.getMessages(username, pageable);
+        Page<Message> messages = logService.getMessages(username, pageable);
+        log.info(
+            messages.getContent().size() > 0
+                ? "Sent logs for: %s. Page %d.".formatted(
+                messages.getContent().get(0).getUsername(),
+                messages.getPageable().getPageNumber())
+                : "No logs found for: " + username);
+        return messages;
     }
 
-
+    @GetMapping(value = "/")
+    @ResponseBody
+    String noUsername(HttpServletResponse response) {
+        response.setStatus(404);
+        return "no username.";
+    }
 }
