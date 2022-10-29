482f6965aec364d71c7c852e2b9d68a102eeef16
Helar Jaadla <helar@jaadla.com>
2022-10-29 07:21:34 +0300

    Remove caching.

--- src/main/java/com/jaadla/namlogapi/NamLogApiApplication.java
+++ src/main/java/com/jaadla/namlogapi/NamLogApiApplication.java
@@ -2,10 +2,8 @@ package com.jaadla.namlogapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class NamLogApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/jaadla/namlogapi/service/LogService.java
+++ src/main/java/com/jaadla/namlogapi/service/LogService.java
@@ -4,11 +4,8 @@ import com.jaadla.namlogapi.model.Message;
 import com.jaadla.namlogapi.repository.MessageRepository;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @CommonsLog
@@ -21,16 +18,9 @@ public class LogService {
     this.messageRepository = messageRepository;
   }
 
-  @Cacheable("messages")
   public Page<Message> getMessages(String username, Pageable pageable) {
 
     return messageRepository.findByUsernameIgnoreCaseOrderByIdAsc(
         username, pageable);
   }
-
-  @Scheduled(cron = "0 0/5 * * * *")
-  @CacheEvict(value = "messages", allEntries = true)
-  public void evictAllCacheValues() {
-  }
-
 }
